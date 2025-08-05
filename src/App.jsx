04cba2552c3bbb94d429fa5e514732996582a9f0
laxman-goud <codewithlaxman92@gmail.com>
2025-08-05 15:05:02 +0530

    addde hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar";
 const App = () => (
   <main className="relative">
     <NavBar/>
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b ">
       <Hero/>
     </section>
     <section className="padding">

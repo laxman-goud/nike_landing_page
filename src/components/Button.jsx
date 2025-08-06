1541d7cbd280e2f0fd216a6e1602812e1f6837d0
laxman-goud <codewithlaxman92@gmail.com>
2025-08-06 07:42:13 +0530

    added special offers section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
-const Button = ({label, iconURL}) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
     return (
-        <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        <button
+            className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full border
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'}`}
+        >
             {label}
-            {iconURL && <img src={iconURL} alt="arrow right" className="ml-2 rounded-full w-5 h-5" />}
+            {iconURL && (
+                <img src={iconURL} alt="arrow right" className="ml-2 rounded-full w-5 h-5" />
+            )}
         </button>
+
     )
 }
 
--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,6 +1,26 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
+
 const SpecialOffers = () => {
     return (
-        <div>SpecialOffers</div>
+        <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+            <div className="flex-1">
+                <img src={offer} alt="offer" width={773} height={687} className="object-contain w-full" />
+            </div>
+
+            <div className="flex flex-1 flex-col">
+                <h2 className="font-palanquin text-4xl capitalize lg:max-w-lg font-bold">
+                    <span className="text-coral-red"> Special</span> Offers
+                </h2>
+                <p className="mt-4 lg:max-w-lg info-text text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia suscipit debitis magni. Lorem ipsum dolor sit amet.</p>
+                <p className="mt-6 lg:max-w-lg info-text text-justify">Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. A eos maiores similique. ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                <div className="mt-11 flex flex-wrap gap-4">
+                    <Button label="Shop now" iconURL={arrowRight}/>
+                    <Button label="Learn more" backgroundColor = "bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" />
+                </div>
+            </div>
+        </section>
     )
 }
 
